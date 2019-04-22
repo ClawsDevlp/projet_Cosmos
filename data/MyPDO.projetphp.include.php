a8ced1939a32c4e1b96f71b4a4c9f317ff7329f4
Unknown <44610840+Julien-Haudegond@users.noreply.github.com>
2019-04-22 19:29:56 +0200

    Ajout des fichiers pour la partie
    Possibilité de créer une partie, lancée une ancienne partie, faire les choix, récupérer les objets et se servir des objets.
    
    Manque : récupération de l'id_joueur, gestion des badges et stats en tout genre du joueur, intégration dans le site.

---
+++ data/MyPDO.projetphp.include.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once 'MyPDO.class.php';
+
+MyPDO::setConfiguration('mysql:host=localhost;dbname=projetphp;charset=utf8', 'root', '');
---
+++ js/partie_home_form.js
@@ -0,0 +1,97 @@
+const button = document.querySelectorAll('.button');
+const startButton = document.querySelector('#button-start');
+const profileButton = document.querySelector('#button-profile');
+let dataContent = "";
+
+//Listeners
+startButton.addEventListener('click', event => startGame(event));
+profileButton.addEventListener('click', event => goToProfile(event));
+
+//Aller sur la page de profil
+const goToProfile = event => {
+	event.preventDefault(); //évite de recharger la page
+	window.location.href = "profile.html";
+}
+
+//Lancer une partie
+const startGame = event => {
+	event.preventDefault(); //évite de recharger la page
+
+	let params = {};
+
+	params[0] = "Me réveiller";
+	console.log(params);
+
+	//Création de l'url et de sa query
+	//Le second paramètre est la racine du site
+	let url = new URL("/projetPHP/src/partie_home.php", "http://localhost");
+	url.search = new URLSearchParams(params);
+	console.log(url);
+
+	//Requête HTTP GET
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+
+			dataContent = data.content;
+
+			if(dataContent == 'Reprendre ?') {
+				if(confirm("Souhaitez-vous reprendre votre aventure là où vous vous étiez arrêté.e ?!")) {
+					let params = {};
+
+					params[0] = "OK";
+					console.log(params);
+
+					//Création de l'url et de sa query
+					//Le second paramètre est la racine du site
+					let url = new URL("/projetPHP/src/partie_home.php", "http://localhost");
+					url.search = new URLSearchParams(params);
+					console.log(url);
+
+					//Requête HTTP GET 
+					fetch(url)
+						.then(response => response.json())
+						.then(data => {
+
+							dataContent = data.content;
+
+						})
+						.catch(error => { console.log(error); });
+				}
+				else {
+					let params = {};
+
+					params[0] = "Annuler";
+					console.log(params);
+
+					//Création de l'url et de sa query
+					//Le second paramètre est la racine du site
+					let url = new URL("/projetPHP/src/partie_home.php", "http://localhost");
+					url.search = new URLSearchParams(params);
+					console.log(url);
+
+					//Requête HTTP GET 
+					fetch(url)
+						.then(response => response.json())
+						.then(data => {
+
+							dataContent = data.content;
+
+						})
+						.catch(error => { console.log(error); });
+				}
+			}
+		})
+		.catch(error => { console.log(error); });			
+}
+
+//Permet de contrer l'asynchronicité de l'éxécution des fonctions
+const waitForGame = () => {
+	if(dataContent == "Continuons !" || dataContent == "Nouvelle partie !") {
+		window.location.href = "partie.html";
+	}
+	else {
+		window.setTimeout(waitForGame, 100);
+	}
+}
+waitForGame();
