c5c88c0e3094e9f65f008e2c500c0e21aeeaf44d
MathildeDumoulin <mathildedumoulin@gmail.com>
2019-05-11 15:59:54 +0200

    Effet défilement texte

--- js/game.js
+++ js/game.js
@@ -9,7 +9,6 @@ const badges = document.getElementById("badges");
 const inventory = document.getElementById("inventory");
 const buttons = document.querySelectorAll("button");
 const buttonsPlus = document.querySelectorAll("button:not(:first-child)");
-
 var id_game;
 
 /*------------------------------
@@ -89,6 +88,7 @@ function makeChoice(evt) {
 
 //Display game
 function displayGame(data) {
+	
     if (!(data.text)) {
         //If end of the game
         buttons[0].innerHTML = "Revenir au menu";
@@ -102,14 +102,14 @@ function displayGame(data) {
 
         let url = new URL("api/game/end_game.php", "http://localhost/projetPHP/");
         url.search = new URLSearchParams(params);
-
+		
         //AJAX query : end game (infos end)
         fetch(url)
             .then(response => {
                 if (response.status == 200) {
                     response.json().then(data => {
                         text.innerHTML = "C'était la fin n°" + data.statistics["nb_end"] + ". Vous avez assisté à " + data.statistics["nb_ends"] + " fin sur 10.";
-
+						typing();
                         if (data.badges) {
                             for (let badge of data.badges) {
                                 let new_img_badge = document.createElement("img");
@@ -133,18 +133,21 @@ function displayGame(data) {
             .catch(error => {
                 console.log(error)
             });
+		
     } else if (!(data.choices)) {
         //If no choices
         buttons[0].dataset.idChoice = 0;
         buttons[0].innerHTML = "Suivant";
         text.innerHTML = data.text["text_content"];
         updateGame(data.text["id_text"]);
+		//console.log("suivant");
     } else {
         //If choices
         for (let i in data.choices) {
             buttons[i].classList.remove("hide");
             buttons[i].dataset.idChoice = data.choices[i]["id_choice"];
             buttons[i].innerHTML = data.choices[i]["text_choice"];
+			
             text.innerHTML = data.text["text_content"];
         }
         updateGame(data.text["id_text"]);
@@ -174,11 +177,13 @@ function updateGame(id_text) {
     params["id_game"] = id_game;
 
     //AJAX query : update a game, insert new text according to player and game
+	
     fetch(url, {
             method: "PUT",
             body: JSON.stringify(params)
         })
         .then(response => {
+		
             if (response.status != 200) {
                 //Error
                 response.json().then(data => {
@@ -192,6 +197,9 @@ function updateGame(id_text) {
         });
 
     url = new URL("api/profile/add_badges_in_game.php", "http://localhost/projetPHP/");
+	
+	//sets typing effects on text
+	typing();
 
     //AJAX query : add badges in game
     fetch(url, {
@@ -210,4 +218,38 @@ function updateGame(id_text) {
         .catch(error => {
             console.log(error)
         });
+	
+}
+
+function typing(){
+
+	let txtContent = text.textContent;
+	let txtLength = txtContent.length;
+	//Empty text content
+	text.innerHTML = "";
+	var counter = 0;
+	
+	//Displaying text content with prompt effect
+	var afficherTexte = setInterval(function(){
+		text.innerHTML += (txtContent.charAt(counter) +"<div id='trait'>A</div>");
+		counter++;
+		if(counter >= txtLength){
+			clearInterval(afficherTexte);
+		}
+		let trait = document.querySelector("#trait");
+		trait.parentNode.removeChild(trait);
+	},20);
+	
+	//Clearing interval when clicking on the text : display whole text
+	text.addEventListener("click",function(){
+		clearInterval(afficherTexte);
+		text.innerHTML = txtContent;
+	});
+	
+	//Clearing interval when clicking on buttons to prevent texts from mixing
+	buttons.forEach(e => {
+		e.addEventListener("click", function(){
+			clearInterval(afficherTexte);
+		});
+	})
 }
